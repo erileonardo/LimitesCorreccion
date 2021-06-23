0edea5edf2c79b16c8043418b20c82aa3fd6cfd5
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-06-23 10:46:50 -0500

    Add files via upload

---
+++ LimitesCorreccion.m
@@ -0,0 +1,19 @@
+% Octave Script
+% Title       : Limites
+% Description : Graficar  
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 23 de junio 2021
+% Version     : 1
+% Notes       : funcion f(x)=x+2
+
+x=-3:0.1:3;
+y=10-x;
+% Dibujar grafica
+plot(x,y)
+grid on; 
+% Titulo
+title("f(x)=x+2");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
